1984723ffaeb751f92711fe3718bde4af19ca3f8
ZYTK Devs <97977626+z1tk@users.noreply.github.com>
2022-03-11 13:09:01 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,51 @@
+function setCookie(cname,cvalue,exdays) {
+    const d = new Date();
+    d.setTime(d.getTime() + (exdays*24*60*60*1000));
+    let expires = "expires=" + d.toUTCString();
+    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+  }
+  
+  function getCookie(cname) {
+    let name = cname + "=";
+    let decodedCookie = decodeURIComponent(document.cookie);
+    let ca = decodedCookie.split(';');
+    for(let i = 0; i < ca.length; i++) {
+      let c = ca[i];
+      while (c.charAt(0) == ' ') {
+        c = c.substring(1);
+      }
+      if (c.indexOf(name) == 0) {
+        return c.substring(name.length, c.length);
+      }
+    }
+    return "";
+  }
+  
+  function checkCookie() {
+    let data = getCookie("data");
+    if (data != "") {
+        let splitData = data.split(":");
+        let name = splitData[0];
+        let uid = splitData[1];
+        let score = splitData[2];
+        alert("Name: " + name + ", User ID: " + uid + ", Score: " + score);
+    }
+  }
+
+var queryString = new Array();
+window.onload = function () {
+    if (queryString.length == 0) {
+        if (window.location.search.split('?').length > 1) {
+            var params = window.location.search.split('?')[1].split('&');
+            for (var i = 0; i < params.length; i++) {
+                var key = params[i].split('=')[0];
+                var value = decodeURIComponent(params[i].split('=')[1]);
+                queryString[key] = value;
+            }
+        }
+    }
+    if (queryString["n"] != null && queryString["uid"] != null && queryString["s"] != null) {
+        setCookie("data", queryString["n"] + ":" + queryString["uid"] + ":" + queryString["s"]);
+        location.href = "./index.html";
+    }
+};
