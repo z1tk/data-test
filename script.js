64878d102f9848fe6cea739e5d55e2bd7c56984c
ZYTK Devs <97977626+z1tk@users.noreply.github.com>
2022-03-11 13:28:28 -0500

    Update script.js

--- script.js
+++ script.js
@@ -32,20 +32,27 @@ function setCookie(cname,cvalue,exdays) {
     }
   }
 
+//   function test() {
+//     let data = "1:2:3";
+//     let splitData = data.split(":");
+//     let name = splitData[0];
+//     let uid = splitData[1];
+//     let score = splitData[2];
+//     alert("Name: " + name + ", User ID: " + uid + ", Score: " + score);
+//   }
+
 var queryString = new Array();
-window.onload = function () {
-    if (queryString.length == 0) {
-        if (window.location.search.split('?').length > 1) {
-            var params = window.location.search.split('?')[1].split('&');
-            for (var i = 0; i < params.length; i++) {
-                var key = params[i].split('=')[0];
-                var value = decodeURIComponent(params[i].split('=')[1]);
-                queryString[key] = value;
-            }
+if (queryString.length == 0) {
+    if (window.location.search.split('?').length > 1) {
+        var params = window.location.search.split('?')[1].split('&');
+        for (var i = 0; i < params.length; i++) {
+            var key = params[i].split('=')[0];
+            var value = decodeURIComponent(params[i].split('=')[1]);
+            queryString[key] = value;
         }
     }
-    if (queryString["n"] != null && queryString["uid"] != null && queryString["s"] != null) {
-        setCookie("data", queryString["n"] + ":" + queryString["uid"] + ":" + queryString["s"], 60);
-        location.href = "./index.html";
-    }
-};
+}
+if (queryString["n"] != null && queryString["uid"] != null && queryString["s"] != null) {
+    setCookie("data", queryString["n"] + ":" + queryString["uid"] + ":" + queryString["s"], 60);
+    location.href = "./index.html";
+}
